e0885d5a48fe793994b1d11a157f9c0826da9c8f
Marco Foscato <marco.foscato@uib.no>
2025-11-13 21:45:51 +0100

    fix strained ring closure examples

--- examples/strained_ring_closure/run.sh
+++ examples/strained_ring_closure/run.sh
@@ -11,27 +11,45 @@ function ensure_file_exists() {
     fi
 }
 
-python ../../src/__main__.py -i c4.int -c 1 11 2 14 --minimize --space-type zmatrix -o c4_min > c4_min.log 2>&1
+python ../../src/__main__.py -i c4.int -c 1 11 2 14 -o c4_min > c4_min.log 2>&1
 
 ensure_file_exists c4_min.xyz
 ensure_file_exists c4_min.int
 
-if ! tail -n 6 c4_min.log | grep -q "Ring closure:.*0\.9[0-9]" ; then
-    echo "Error: Ring closure score not found or incorrect"
+if ! tail -n 6 c4_min.log | grep -q "Final ring closure score:.*0\.9" ; then
+    echo "Error: Ring closure score not found or incorrect for c4"
     exit 1
 fi
 
 n=$(diff c4.int c4_min.int  | grep -cE "^[<>]")
+n=$((n/2))
+if [ $n != 7 ]; then
+    echo "Error: Expected 7 lines changed, found $n"
+    exit 1
+fi
+
+echo "Distorted c4 chain closure completed successfully"
+
+python ../../src/__main__.py -i c5.int -c 1 6 2 7 -o c5_min > c5_min.log 2>&1
+
+ensure_file_exists c5_min.xyz
+ensure_file_exists c5_min.int
+
+if ! tail -n 6 c5_min.log | grep -q "Final ring closure score:.*0\.9[0-9]" ; then
+    echo "Error: Ring closure score not found or incorrect for c5"
+    exit 1
+fi
+
+n=$(diff c5.int c5_min.int  | grep -cE "^[<>]")
 n=$((n/2))
 if [ $n != 5 ]; then
     echo "Error: Expected 5 lines changed, found $n"
     exit 1
 fi
 
+echo "Distorted c5 chain closure completed successfully"
 
-echo "Distorted c4 chain closure completed successfully"
-
-python ../../src/__main__.py -i distorted_complex.int -r 1 2 5 31 31 32 32 35 35 40 35 41 32 36 32 37 -c 7 39 77 35 -o distorted_complex_min --minimize --space-type zmatrix > distorted_complex_min.log 2>&1
+python ../../src/__main__.py -i distorted_complex.int -r 1 2 5 31 31 32 32 35 35 40 35 41 32 36 32 37 -c 7 39 77 35 -o distorted_complex_min  > distorted_complex_min.log 2>&1
 
 ensure_file_exists distorted_complex_min.xyz
 ensure_file_exists distorted_complex_min.int
@@ -39,13 +57,13 @@ ensure_file_exists distorted_complex_min.int
 # Twice the number of lines changed
 n=$(diff distorted_complex.int distorted_complex_min.int  | grep -cE "^[<>]")
 n=$((n/2))
-if [ $n != 8 ]; then
-    echo "Error: Expected 8 lines changed, found $n"
+if [ $n != 10 ]; then
+    echo "Error: Expected 10 lines changed, found $n"
     exit 1
 fi
 
-if ! tail -n 10 distorted_complex_min.log | grep -q "Ring closure:.*0\.9[0-9]" ; then
-    echo "Error: Ring closure score not found or incorrect"
+if ! tail -n 10 distorted_complex_min.log | grep -q "Final ring closure score:.*0\.9[0-9]" ; then
+    echo "Error: Ring closure score not found or incorrect for complex"
     exit 1
 fi
 
